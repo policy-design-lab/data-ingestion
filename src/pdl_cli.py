ec5b422994e4c286464779649ea7f68e80a3cf92
Sandeep Puthanveetil Satheesan <sandeeps@illinois.edu>
2024-07-22 14:46:20 -0500

    Main to develop after 0.1.0 (#20)
    * Add CHANGELOG and update version number.
    
    * Add feature to support versioning.

--- src/pdl_cli.py
+++ src/pdl_cli.py
@@ -1,5 +1,6 @@
 import argparse
 import logging
+from config import VERSION
 
 
 class PolicyDesignLabDataCLI:
@@ -40,6 +41,8 @@ class PolicyDesignLabDataCLI:
         self.parser.add_argument('--init-tables', '-i', action='store_true', help='Initialize tables', default=False)
         self.parser.add_argument('--insert-data', '-I', action='store_true', help='Insert data',
                                  default=False)
+        # Version
+        self.parser.add_argument('--version', action='version', version='PDL Data Ingestion CLI ' + VERSION)
         self.args = self.parser.parse_args()
         return
 
