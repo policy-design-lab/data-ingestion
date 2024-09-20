79895998ccff13537aa6349a453962735eef856f
Sandeep Puthanveetil Satheesan <sandeeps@illinois.edu>
2024-09-20 15:09:00 -0500

    Code formatting.

--- queries/create_tables.sql
+++ queries/create_tables.sql
@@ -5,8 +5,8 @@ BEGIN;
 
 CREATE TABLE IF NOT EXISTS pdl.titles
 (
-    id smallint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 100 MINVALUE 100 MAXVALUE 1000 CACHE 1 ),
-    name character varying(100) NOT NULL,
+    id          smallint               NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 100 MINVALUE 100 MAXVALUE 1000 CACHE 1 ),
+    name        character varying(100) NOT NULL,
     description text,
     CONSTRAINT pk_titles PRIMARY KEY (id),
     CONSTRAINT uc_title_name UNIQUE (name)
@@ -14,83 +14,83 @@ CREATE TABLE IF NOT EXISTS pdl.titles
 
 CREATE TABLE IF NOT EXISTS pdl.programs
 (
-    id smallint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 100 MINVALUE 100 MAXVALUE 1000 CACHE 1 ),
-    title_id smallint,
+    id          smallint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 100 MINVALUE 100 MAXVALUE 1000 CACHE 1 ),
+    title_id    smallint,
     subtitle_id smallint,
-    name character varying(100),
+    name        character varying(100),
     CONSTRAINT pk_programs PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS pdl.sub_programs
 (
-    id smallint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 100 MINVALUE 100 MAXVALUE 10000 CACHE 1 ),
+    id         smallint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 100 MINVALUE 100 MAXVALUE 10000 CACHE 1 ),
     program_id smallint NOT NULL,
-    name character varying(100),
+    name       character varying(100),
     CONSTRAINT pk_sub_programs PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS pdl.payments
 (
-    id bigint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 100 MINVALUE 100 MAXVALUE 1000000 CACHE 1 ),
-    title_id smallint,
-    subtitle_id smallint,
-    program_id smallint,
-    sub_program_id smallint,
-    sub_sub_program_id smallint,
-    practice_category_id smallint,
-    state_code character varying(2) NOT NULL,
-    year smallint NOT NULL,
-    payment numeric(14, 2),
-    recipient_count bigint,
-    base_acres numeric(10, 2),
-    farm_count bigint,
-    contract_count bigint,
-    premium_policy_count bigint,
-    liability_amount bigint,
-    premium_amount bigint,
-    premium_subsidy_amount bigint,
-    indemnity_amount bigint,
-    farmer_premium_amount bigint,
-    loss_ratio numeric,
+    id                        bigint               NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 100 MINVALUE 100 MAXVALUE 1000000 CACHE 1 ),
+    title_id                  smallint,
+    subtitle_id               smallint,
+    program_id                smallint,
+    sub_program_id            smallint,
+    sub_sub_program_id        smallint,
+    practice_category_id      smallint,
+    state_code                character varying(2) NOT NULL,
+    year                      smallint             NOT NULL,
+    payment                   numeric(14, 2),
+    recipient_count           bigint,
+    base_acres                numeric(10, 2),
+    farm_count                bigint,
+    contract_count            bigint,
+    premium_policy_count      bigint,
+    liability_amount          bigint,
+    premium_amount            bigint,
+    premium_subsidy_amount    bigint,
+    indemnity_amount          bigint,
+    farmer_premium_amount     bigint,
+    loss_ratio                numeric,
     net_farmer_benefit_amount bigint,
-    practice_code character varying(100),
-    practice_code_variant character varying(100),
+    practice_code             character varying(100),
+    practice_code_variant     character varying(100),
     CONSTRAINT pk_payments PRIMARY KEY (id),
     CONSTRAINT uc_payments UNIQUE (title_id, subtitle_id, program_id, sub_program_id, state_code, year)
 );
 
 CREATE TABLE IF NOT EXISTS pdl.states
 (
-    code character varying(2) NOT NULL,
+    code character varying(2)   NOT NULL,
     name character varying(100) NOT NULL,
     CONSTRAINT pk_states PRIMARY KEY (code)
 );
 
 CREATE TABLE IF NOT EXISTS pdl.subtitles
 (
-    id smallint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 100 MINVALUE 100 MAXVALUE 1000 ),
-    title_id smallint NOT NULL,
-    name character varying(100) NOT NULL,
+    id       smallint               NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 100 MINVALUE 100 MAXVALUE 1000 ),
+    title_id smallint               NOT NULL,
+    name     character varying(100) NOT NULL,
     CONSTRAINT pk_subtitles PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS pdl.practice_categories
 (
-    id smallint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 100 MINVALUE 100 MAXVALUE 1000 ),
-    name character varying NOT NULL,
-    display_name character varying,
-    category_grouping character varying,
-    program_id smallint NOT NULL,
-    is_statutory_category boolean NOT NULL,
+    id                    smallint          NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 100 MINVALUE 100 MAXVALUE 1000 ),
+    name                  character varying NOT NULL,
+    display_name          character varying,
+    category_grouping     character varying,
+    program_id            smallint          NOT NULL,
+    is_statutory_category boolean           NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS pdl.practices
 (
-    code character varying(100) NOT NULL,
-    name character varying(200) NOT NULL,
+    code         character varying(100) NOT NULL,
+    name         character varying(200) NOT NULL,
     display_name character varying(200),
-    source character varying,
+    source       character varying,
     CONSTRAINT pk_practices PRIMARY KEY (code)
 );
 
@@ -99,121 +99,121 @@ COMMENT ON TABLE pdl.practices
 
 CREATE TABLE IF NOT EXISTS pdl.sub_sub_programs
 (
-    id smallint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 100 MINVALUE 100 MAXVALUE 10000 ),
+    id             smallint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 100 MINVALUE 100 MAXVALUE 10000 ),
     sub_program_id smallint NOT NULL,
-    name character varying(100),
+    name           character varying(100),
     CONSTRAINT pk_sub_sub_programs PRIMARY KEY (id)
 );
 
 ALTER TABLE IF EXISTS pdl.programs
     ADD CONSTRAINT "fk_ titles_id" FOREIGN KEY (title_id)
-    REFERENCES pdl.titles (id) MATCH SIMPLE
-    ON UPDATE NO ACTION
-    ON DELETE CASCADE
-    NOT VALID;
+        REFERENCES pdl.titles (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE CASCADE
+        NOT VALID;
 
 
 ALTER TABLE IF EXISTS pdl.programs
     ADD CONSTRAINT "fk_ subtitles_id" FOREIGN KEY (subtitle_id)
-    REFERENCES pdl.subtitles (id) MATCH SIMPLE
-    ON UPDATE NO ACTION
-    ON DELETE CASCADE
-    NOT VALID;
+        REFERENCES pdl.subtitles (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE CASCADE
+        NOT VALID;
 
 
 ALTER TABLE IF EXISTS pdl.sub_programs
     ADD CONSTRAINT fk_programs_id FOREIGN KEY (program_id)
-    REFERENCES pdl.programs (id) MATCH SIMPLE
-    ON UPDATE NO ACTION
-    ON DELETE CASCADE
-    NOT VALID;
+        REFERENCES pdl.programs (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE CASCADE
+        NOT VALID;
 
 
 ALTER TABLE IF EXISTS pdl.payments
     ADD CONSTRAINT fk_programs_id FOREIGN KEY (program_id)
-    REFERENCES pdl.programs (id) MATCH SIMPLE
-    ON UPDATE NO ACTION
-    ON DELETE CASCADE
-    NOT VALID;
+        REFERENCES pdl.programs (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE CASCADE
+        NOT VALID;
 
 
 ALTER TABLE IF EXISTS pdl.payments
     ADD CONSTRAINT fk_sub_programs_id FOREIGN KEY (sub_program_id)
-    REFERENCES pdl.sub_programs (id) MATCH SIMPLE
-    ON UPDATE NO ACTION
-    ON DELETE CASCADE
-    NOT VALID;
+        REFERENCES pdl.sub_programs (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE CASCADE
+        NOT VALID;
 
 
 ALTER TABLE IF EXISTS pdl.payments
     ADD CONSTRAINT fk_states_id FOREIGN KEY (state_code)
-    REFERENCES pdl.states (code) MATCH SIMPLE
-    ON UPDATE NO ACTION
-    ON DELETE CASCADE
-    NOT VALID;
+        REFERENCES pdl.states (code) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE CASCADE
+        NOT VALID;
 
 
 ALTER TABLE IF EXISTS pdl.payments
     ADD CONSTRAINT fk_titles_id FOREIGN KEY (title_id)
-    REFERENCES pdl.titles (id) MATCH SIMPLE
-    ON UPDATE NO ACTION
-    ON DELETE NO ACTION
-    NOT VALID;
+        REFERENCES pdl.titles (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID;
 
 
 ALTER TABLE IF EXISTS pdl.payments
     ADD CONSTRAINT fk_subtitles_id FOREIGN KEY (subtitle_id)
-    REFERENCES pdl.subtitles (id) MATCH SIMPLE
-    ON UPDATE NO ACTION
-    ON DELETE NO ACTION
-    NOT VALID;
+        REFERENCES pdl.subtitles (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID;
 
 
 ALTER TABLE IF EXISTS pdl.payments
     ADD CONSTRAINT fk_practice_categories_id FOREIGN KEY (practice_category_id)
-    REFERENCES pdl.practice_categories (id) MATCH SIMPLE
-    ON UPDATE NO ACTION
-    ON DELETE NO ACTION
-    NOT VALID;
+        REFERENCES pdl.practice_categories (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID;
 
 
 ALTER TABLE IF EXISTS pdl.payments
     ADD CONSTRAINT fk_practice_code FOREIGN KEY (practice_code)
-    REFERENCES pdl.practices (code) MATCH SIMPLE
-    ON UPDATE NO ACTION
-    ON DELETE NO ACTION
-    NOT VALID;
+        REFERENCES pdl.practices (code) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID;
 
 
 ALTER TABLE IF EXISTS pdl.payments
     ADD CONSTRAINT fk_sub_sub_programs_id FOREIGN KEY (sub_sub_program_id)
-    REFERENCES pdl.sub_sub_programs (id) MATCH SIMPLE
-    ON UPDATE NO ACTION
-    ON DELETE NO ACTION
-    NOT VALID;
+        REFERENCES pdl.sub_sub_programs (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID;
 
 
 ALTER TABLE IF EXISTS pdl.subtitles
     ADD CONSTRAINT fk_titles_id FOREIGN KEY (title_id)
-    REFERENCES pdl.titles (id) MATCH SIMPLE
-    ON UPDATE NO ACTION
-    ON DELETE CASCADE
-    NOT VALID;
+        REFERENCES pdl.titles (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE CASCADE
+        NOT VALID;
 
 
 ALTER TABLE IF EXISTS pdl.practice_categories
     ADD CONSTRAINT fk_program_id FOREIGN KEY (program_id)
-    REFERENCES pdl.programs (id) MATCH SIMPLE
-    ON UPDATE NO ACTION
-    ON DELETE NO ACTION
-    NOT VALID;
+        REFERENCES pdl.programs (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID;
 
 
 ALTER TABLE IF EXISTS pdl.sub_sub_programs
     ADD CONSTRAINT fk_sub_programs_id FOREIGN KEY (sub_program_id)
-    REFERENCES pdl.sub_programs (id) MATCH SIMPLE
-    ON UPDATE NO ACTION
-    ON DELETE NO ACTION
-    NOT VALID;
+        REFERENCES pdl.sub_programs (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID;
 
 END;
