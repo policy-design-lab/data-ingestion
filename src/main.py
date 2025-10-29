55750f6dd2b021b62f41ac7f81f0f5a81cf69ee2
Ya-Lan Yang <63822845+ylyangtw@users.noreply.github.com>
2025-10-29 09:18:11 -0500

    Update crop insurance state level data for 2014-2024 (#85)

--- src/main.py
+++ src/main.py
@@ -61,9 +61,9 @@ if __name__ == '__main__':
                                   snap_cost_filename="snap_costs.csv")
     snap_data_parser.format_data()
 
-    crop_insurance_data_parser = DataParser(2014, 2023, "Crop Insurance",
+    crop_insurance_data_parser = DataParser(2014, 2024, "Crop Insurance",
                                             "../data/crop-insurance", "",
-                                            ci_state_year_benefit_filename="ci_state_year_benefits 2014-2023.csv")
+                                            ci_state_year_benefit_filename="ci_state_year_benefits 2014-2024.csv")
     crop_insurance_data_parser.format_data()
 
     if cli.args.insert_data:
